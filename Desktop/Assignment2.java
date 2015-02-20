de5053602b62e628991ceedb1fb0314cb45ac0bd
瑞骞 姚 <ruiqianyao@Ryan-yaos-MacBook-Pro.local>
2015-02-20 16:13:16 -0500

    The second part of assignment2

---
+++ Desktop/Assignment2.java
@@ -0,0 +1,28 @@
+public class Assignment2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int a,b,c,d;
+    double e,f,g;
+    String h;
+    a=1/2;
+    b=1%2;
+    e=1.0/2;
+    c=5+7/2;
+    h="Beat" + ' ' + "Army";
+    d=6 + 13 / 5 - 35 % 3;
+    f=3.5 * (5/4);
+    g=(3.5 * 5)/4;
+    System.out.println("The answer for int 1 / 2 is "+a+
+    		           "\nThe answer for int 1 % 2 is "+b+
+    		           "\nThe answer for int 5 + 7 / 2 is "+c+
+    		           "\nThe answer for int 6 + 13 / 5 - 35 % 3 is "+d+
+    		           "\nThe answer for double 1.0 / 2 is "+e+
+    		           "\nThe answer for double 3.5 * (5/4) is "+f+
+    		           "\nThe answer for double (3.5 * 5)/4 is "+g+
+    		           "\nThe answer for String Beat + ' ' + Army is "+h);
+    
+    
+	}
+
+}
